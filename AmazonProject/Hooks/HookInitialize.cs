65ec68d55feef5b2f7dc75d83ca05a499332d72d
Vemuri Naveen <vemurinaveen069@gmail.com>
2022-03-11 15:37:22 +0530

    Second commit

---
+++ AmazonProject/Hooks/HookInitialize.cs
@@ -0,0 +1,127 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using SeleniumFramework.Base;
+using AventStack.ExtentReports.Gherkin.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace AmazonProject.Hooks
+{
+    [Binding]
+    public  class HookInitialize: TestInitializeHooks
+    {
+        private readonly DriverConfig _driverConfig;
+        private readonly FeatureContext _featureContext;
+        private readonly ScenarioContext _scenarioContext;
+        private ExtentTest _currentScenarioName;
+
+
+        public HookInitialize(DriverConfig driverConfig, FeatureContext featureContext, ScenarioContext scenarioContext) : base(driverConfig)
+        {
+            _driverConfig = driverConfig;
+            _featureContext = featureContext;
+            _scenarioContext = scenarioContext;
+        }
+
+
+
+        private static ExtentTest featureName;
+        private static ExtentReports extent;
+        private static ExtentKlovReporter klov;
+
+
+        [AfterStep]
+        public void AfterEachStep()
+        {
+
+            var stepType = _scenarioContext.StepContext.StepInfo.StepDefinitionType.ToString();
+
+            if (_scenarioContext.TestError == null)
+            {
+                if (stepType == "Given")
+                    _currentScenarioName.CreateNode<Given>(_scenarioContext.StepContext.StepInfo.Text);
+                else if (stepType == "When")
+                    _currentScenarioName.CreateNode<When>(_scenarioContext.StepContext.StepInfo.Text);
+                else if (stepType == "Then")
+                    _currentScenarioName.CreateNode<Then>(_scenarioContext.StepContext.StepInfo.Text);
+                else if (stepType == "And")
+                    _currentScenarioName.CreateNode<And>(_scenarioContext.StepContext.StepInfo.Text);
+            }
+            else if (_scenarioContext.TestError != null)
+            {
+                //screenshot in the Base64 format
+                var mediaEntity = _driverConfig.CaptureScreenshotAndReturnModel(_scenarioContext.ScenarioInfo.Title.Trim());
+
+                if (stepType == "Given")
+                    _currentScenarioName.CreateNode<Given>(_scenarioContext.StepContext.StepInfo.Text).Fail(_scenarioContext.TestError.Message, mediaEntity);
+                else if (stepType == "When")
+                    _currentScenarioName.CreateNode<When>(_scenarioContext.StepContext.StepInfo.Text).Fail(_scenarioContext.TestError.Message, mediaEntity);
+                else if (stepType == "Then")
+                    _currentScenarioName.CreateNode<Then>(_scenarioContext.StepContext.StepInfo.Text).Fail(_scenarioContext.TestError.Message, mediaEntity);
+            }
+            else if (_scenarioContext.ScenarioExecutionStatus.ToString() == "StepDefinitionPending")
+            {
+                if (stepType == "Given")
+                    _currentScenarioName.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text).Skip("Step Definition Pending");
+                else if (stepType == "When")
+                    _currentScenarioName.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text).Skip("Step Definition Pending");
+                else if (stepType == "Then")
+                    _currentScenarioName.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text).Skip("Step Definition Pending");
+
+            }
+        }
+
+        [BeforeTestRun]
+        public static void TestInitalize()
+        {
+            
+            //Initialize Extent report before test starts
+            var htmlReporter = new ExtentHtmlReporter(@"C:\extentreport\SeleniumWithSpecflow\SpecflowParallelTest\ExtentReport.html");
+            htmlReporter.Config.Theme = AventStack.ExtentReports.Reporter.Configuration.Theme.Dark;
+            //Attach report to reporter
+            extent = new AventStack.ExtentReports.ExtentReports();
+            klov = new ExtentKlovReporter();
+
+
+
+            extent.AttachReporter(htmlReporter);
+        }
+
+
+        [BeforeScenario]
+        public void Initialize()
+        {
+            InitializeSettings();
+            
+
+            //Get feature Name
+            featureName = extent.CreateTest<Feature>(_featureContext.FeatureInfo.Title);
+
+            //Create dynamic scenario name
+            _currentScenarioName = featureName.CreateNode<Scenario>(_scenarioContext.ScenarioInfo.Title);
+        }
+
+
+
+        [AfterScenario]
+        public void TestStop()
+        {
+            //DriverContext.Driver.Quit();
+            //Flush report once test completes
+            _driverConfig.Driver.Quit();
+        }
+
+        [AfterTestRun]
+        public static void TearDownReport()
+        {
+            //Flush report once test completes
+            extent.Flush();
+
+        }
+
+
+    }
+}
+}
